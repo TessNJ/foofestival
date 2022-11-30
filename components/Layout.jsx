9899c4dc949f0b499e28428d715e8c2f4185cb4e
Tess <tere0506@stud.kea.dk>
2022-11-30 15:30:24 +0100

    pages_initial-setup

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Layout({ children }) {
   return (
     <>
-      <header>header content (Layout.jsx)</header>
+      <header>
+        <div>
+          <Image alt=""></Image>
+          <h3>FooFestival</h3>
+        </div>
+        <div></div>
+      </header>
       <main>{children}</main>
-      <footer>footer content (Layout.jsx)</footer>
+      <footer>
+        <div>
+          <Image alt=""></Image>
+          <h5>FooFestival</h5>
+        </div>
+        <div>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </footer>
     </>
   );
 }
